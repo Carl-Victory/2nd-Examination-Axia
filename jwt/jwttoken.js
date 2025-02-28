a585806cc2749133d6d3cc0cdb962a64ccc246de
victory nnamdi <nnamdivictory0@gmail.com>
2025-02-28 23:24:52 +0100

    Updated feature

--- jwt/jwttoken.js
+++ jwt/jwttoken.js
@@ -1,8 +1,10 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'); //We require jwt, a module from installed dependencies
 
+//Create token
 const jwttoken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: '1d'}
     )};
 
+    //Export token
     module.exports = jwttoken;
--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -1,33 +1,49 @@
+//Firstly, We require jwt, a module from installed dependencies
 const jwt = require('jsonwebtoken');
+
+//We require our user schema 
 const userSchema = require('../models/usermodel');
+
+//We require our dot env
 require('dotenv').config();
 
-const authmiddleware = async (req, res, next) => {
-    const token = req.cookies.token; // Get token from cookies (used in web apps)
 
+const authmiddleware = async (req, res, next) => {
+    // Get token from cookies (used in web apps)
+    const token = req.cookies.token; 
+
+    // We throw in an error if Token not found
     if (!token) {
-        return res.status(401).json({ message: 'Please login to continue' }); // Token not found
+        return res.status(401).json({ message: 'Please login to continue' }); 
     }
 
     try {
-        const verifiedToken = jwt.verify(token, process.env.JWT_SECRET); // Verify token
+        // Verify token
+        const verifiedToken = jwt.verify(token, process.env.JWT_SECRET); 
 
+        // Return error if token is invalid
         if (!verifiedToken) {
-            return res.status(403).json({ message: 'Invalid token' }); // Return if token is invalid
+            return res.status(403).json({ message: 'Invalid token' }); 
         }
+        // Get user without password
+        const user = await userSchema.findById(verifiedToken.id).select('-password'); 
 
-        const user = await userSchema.findById(verifiedToken.id).select('-password'); // Get user without password
-
+        // Return if user is not found
         if (!user) {
-            return res.status(404).json({ message: 'User not found' }); // Return if user is not found
+            return res.status(404).json({ message: 'User not found' }); 
         }
 
-        req.user = user; // Attach user to request
-        next(); // Continue to next middleware or route handler
+        // Attach user to request
+        req.user = user; 
+
+        // Continue to next middleware or route handler
+        next(); 
     } catch (error) {
-        console.error(error);
+        console.error(error);//If process fails, display in our terminal
+        //if process fails, respond with error status code and message to the client
         res.status(500).json({ message: 'Server error' }); // Handle errors
     }
 };
 
+//Export created middleware
 module.exports = authmiddleware;
